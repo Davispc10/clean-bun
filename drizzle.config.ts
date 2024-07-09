aa73d6bf7c9d953b1937ab0d5a5e74345d753f5a
David Azeredo <davispc100@gmail.com>
2024-07-08 21:43:47 -0300

    fix: create migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'drizzle-kit'
 
 export default {
-  schema: './src/db/schema/index.ts',
-  out: '/drizzle',
+  schema: './src/db/drizzle/schema/index.ts',
+  out: './src/db/drizzle/sql',
   dialect: 'postgresql',
 } satisfies Config;
