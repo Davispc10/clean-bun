2c093e41e4e3dae7d7d6becac1817dda413f26e0
David Azeredo <davispc100@gmail.com>
2024-07-10 23:45:59 +0000

    feat: create getProfile and getRestaurant

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,12 +3,16 @@ import { registerRestaurant } from './routes/register-restaurant';
 import { sendAuthLink } from './routes/send-auth-link';
 import { authenticateFromLink } from './routes/authenticate-from-link';
 import { signOut } from './routes/sign-out';
+import { getProfile } from './routes/get-profile';
+import { getManagedRestaurant } from './routes/get-managed-restaurant';
 
 const app = new Elysia()
   .use(registerRestaurant)
   .use(sendAuthLink)
   .use(authenticateFromLink)
-  .use(signOut);
+  .use(signOut)
+  .use(getProfile)
+  .use(getManagedRestaurant);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
