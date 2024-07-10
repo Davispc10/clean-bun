4f4a082a903e5f161138f34960c33451965e2790
David Azeredo <davispc100@gmail.com>
2024-07-10 22:42:18 +0000

    feat: token validation and set token on cookie

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,20 +1,12 @@
-import { Elysia, t } from 'elysia';
+import { Elysia } from 'elysia';
 import { registerRestaurant } from './routes/register-restaurant';
 import { sendAuthLink } from './routes/send-auth-link';
-import jwt from '@elysiajs/jwt';
-import { env } from '../env';
-import cookie from '@elysiajs/cookie';
+import { authenticateFromLink } from './routes/authenticate-from-link';
 
 const app = new Elysia()
-  .use(jwt({
-    secret: env.JWT_SECRET_KEY,
-    schema: t.Object({
-      sub: t.String(),
-      restaurantId: t.Optional(t.String()),
-    })
-  }))
   .use(registerRestaurant)
   .use(sendAuthLink)
+  .use(authenticateFromLink)
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
